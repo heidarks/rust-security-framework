97c861f2803522bcfc7694b2aa27844a6d6ca3a6
heidarks <heidarks@users.noreply.github.com>
2024-09-20 10:35:16 +0000

    software will search both..

--- security-framework/src/item.rs
+++ security-framework/src/item.rs
@@ -370,23 +370,20 @@ impl ItemSearchOptions {
                 );
             }
 
-            match self.token.as_ref().unwrap_or(&Token::Software) {
-                Token::Software => {
-                    println!("token: software");
-                    params.add(
-                        &kSecAttrTokenID.to_void(),
-                        &kSecAttrTokenIDSecureElement.to_void(),
-                    );
-                },
-                Token::SecureEnclave => {
-                    println!("token: secure enclave");
-                    params.add(
-                        &kSecAttrTokenID.to_void(),
-                        &kSecAttrTokenIDSecureEnclave.to_void(),
-                    );
+            if let Some(ref token) = self.token {
+                match token {
+                    Token::Software => {
+                        println!("token: software");
+                    },
+                    Token::SecureEnclave => {
+                        println!("token: secure enclave");
+                        params.add(
+                            &kSecAttrTokenID.to_void(),
+                            &kSecAttrTokenIDSecureEnclave.to_void(),
+                        );
+                    }
                 }
             }
-
 
             if let Some(ref service) = self.service {
                 params.add(
